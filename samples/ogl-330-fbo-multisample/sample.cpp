0a32f48c43186039ca6be06f321533d377fa7419
Christophe Riccio <g.truc.creation@gmail.com>
2010-06-07 13:47:15 +0100

    update samples

--- samples/ogl-330-fbo-multisample/sample.cpp
+++ samples/ogl-330-fbo-multisample/sample.cpp
@@ -1,5 +1,5 @@
 //**********************************
-// OpenGL Framebuffer multisample
+// OpenGL Framebuffer Multisample
 // 01/09/2009
 //**********************************
 // Christophe Riccio
@@ -13,7 +13,7 @@
 
 namespace
 {
-	std::string const SAMPLE_NAME = "OpenGL Framebuffer multisample";	
+	std::string const SAMPLE_NAME = "OpenGL Framebuffer Multisample";	
 	std::string const VERTEX_SHADER_SOURCE(glf::DATA_DIRECTORY + "330/image-2d.vert");
 	std::string const FRAGMENT_SHADER_SOURCE(glf::DATA_DIRECTORY + "330/image-2d.frag");
 	std::string const TEXTURE_DIFFUSE(glf::DATA_DIRECTORY + "kueken320-rgb8.tga");
