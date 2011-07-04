b1501342c6932dd923b569812ac65c9afaf94155
Christophe Riccio <mail@g-truc.net>
2011-07-04 23:49:26 +0100

    Completed OpenGL 4.2 pixel store sample

---
+++ samples/ogl-420-texture-2d-pixel-store.cpp
@@ -0,0 +1,235 @@
+//**********************************
+// OpenGL Texture 2D Compressed
+// 02/07/2011 - 03/07/2011
+//**********************************
+// Christophe Riccio
+// ogl-samples@g-truc.net
+//**********************************
+// G-Truc Creation
+// www.g-truc.net
+//**********************************
+
+#include <glf/glf.hpp>
+#include <gli/gtx/gl_texture2d.hpp>
+
+namespace
+{
+	std::string const SAMPLE_NAME = "OpenGL Texture 2D Compressed";
+	std::string const SHADER_VERT_SOURCE(glf::DATA_DIRECTORY + "420/texture-2d.vert");
+	std::string const SHADER_FRAG_SOURCE(glf::DATA_DIRECTORY + "420/texture-2d.frag");
+	std::string const TEXTURE_DIFFUSE_BC1(glf::DATA_DIRECTORY + "kueken256-bc1.dds");
+
+	int const SAMPLE_SIZE_WIDTH(640);
+	int const SAMPLE_SIZE_HEIGHT(480);
+	int const SAMPLE_MAJOR_VERSION(4);
+	int const SAMPLE_MINOR_VERSION(1);
+
+	glf::window Window(glm::ivec2(SAMPLE_SIZE_WIDTH, SAMPLE_SIZE_HEIGHT));
+
+	struct vertex
+	{
+		vertex
+		(
+			glm::vec2 const & Position,
+			glm::vec2 const & Texcoord
+		) :
+			Position(Position),
+			Texcoord(Texcoord)
+		{}
+
+		glm::vec2 Position;
+		glm::vec2 Texcoord;
+	};
+
+	// With DDS textures, v texture coordinate are reversed, from top to bottom
+	GLsizei const VertexCount(6);
+	GLsizeiptr const VertexSize = VertexCount * sizeof(vertex);
+	vertex const VertexData[VertexCount] =
+	{
+		vertex(glm::vec2(-1.0f,-1.0f), glm::vec2(0.0f, 1.0f)),
+		vertex(glm::vec2( 1.0f,-1.0f), glm::vec2(1.0f, 1.0f)),
+		vertex(glm::vec2( 1.0f, 1.0f), glm::vec2(1.0f, 0.0f)),
+		vertex(glm::vec2( 1.0f, 1.0f), glm::vec2(1.0f, 0.0f)),
+		vertex(glm::vec2(-1.0f, 1.0f), glm::vec2(0.0f, 0.0f)),
+		vertex(glm::vec2(-1.0f,-1.0f), glm::vec2(0.0f, 1.0f))
+	};
+
+	GLuint VertexArrayName(0);
+	GLuint ProgramName(0);
+
+	GLuint BufferName(0);
+	GLuint Texture2DName(0);
+
+	GLint UniformMVP(0);
+	GLint UniformDiffuse(0);
+
+}//namespace
+
+bool initProgram()
+{
+	bool Validated = true;
+	
+	if(Validated)
+	{
+		GLuint VertShaderName = glf::createShader(GL_VERTEX_SHADER, SHADER_VERT_SOURCE);
+		GLuint FragShaderName = glf::createShader(GL_FRAGMENT_SHADER, SHADER_FRAG_SOURCE);
+
+		ProgramName = glCreateProgram();
+		glAttachShader(ProgramName, VertShaderName);
+		glAttachShader(ProgramName, FragShaderName);
+		glDeleteShader(VertShaderName);
+		glDeleteShader(FragShaderName);
+
+		glLinkProgram(ProgramName);
+		Validated = glf::checkProgram(ProgramName);
+	}
+
+	if(Validated)
+	{
+		UniformMVP = glGetUniformLocation(ProgramName, "MVP");
+		UniformDiffuse = glGetUniformLocation(ProgramName, "Diffuse");
+	}
+
+	return glf::checkError("initProgram");
+}
+
+bool initArrayBuffer()
+{
+	glGenBuffers(1, &BufferName);
+
+    glBindBuffer(GL_ARRAY_BUFFER, BufferName);
+    glBufferData(GL_ARRAY_BUFFER, VertexSize, VertexData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	return glf::checkError("initArrayBuffer");;
+}
+
+bool initTexture2D()
+{
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glPixelStorei(GL_UNPACK_COMPRESSED_BLOCK_WIDTH, 4);
+	glPixelStorei(GL_UNPACK_COMPRESSED_BLOCK_HEIGHT, 4);
+	glPixelStorei(GL_UNPACK_COMPRESSED_BLOCK_DEPTH, 1);
+	glPixelStorei(GL_UNPACK_COMPRESSED_BLOCK_SIZE, 64);
+
+	glGenTextures(1, &Texture2DName);
+
+	gli::texture2D Texture = gli::load(TEXTURE_DIFFUSE_BC1);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Texture2DName);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1000);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_R, GL_RED);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_G, GL_GREEN);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_B, GL_BLUE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_SWIZZLE_A, GL_ALPHA);
+
+	for(std::size_t Level = 0; Level < Texture.levels(); ++Level)
+	{
+		glPixelStorei(GL_UNPACK_ROW_LENGTH, Texture[Level].dimensions().x);
+		glPixelStorei(GL_UNPACK_SKIP_PIXELS, GLsizei(Texture[Level].dimensions().x) / 4);
+		glPixelStorei(GL_UNPACK_SKIP_ROWS, GLsizei(Texture[Level].dimensions().y) / 4);
+
+		glCompressedTexImage2D(
+			GL_TEXTURE_2D,
+			GLint(Level),
+			GL_COMPRESSED_RGB_S3TC_DXT1_EXT,
+			GLsizei(Texture[Level].dimensions().x) / 1, 
+			GLsizei(Texture[Level].dimensions().y) / 1, 
+			0, 
+			GLsizei(Texture[Level].capacity()), 
+			Texture[Level].data());
+	}
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	return glf::checkError("initTexture2D");
+}
+
+bool initVertexArray()
+{
+	glGenVertexArrays(1, &VertexArrayName);
+    glBindVertexArray(VertexArrayName);
+		glBindBuffer(GL_ARRAY_BUFFER, BufferName);
+		glVertexAttribPointer(glf::semantic::attr::POSITION, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), GLF_BUFFER_OFFSET(0));
+		glVertexAttribPointer(glf::semantic::attr::TEXCOORD, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), GLF_BUFFER_OFFSET(sizeof(glm::vec2)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glEnableVertexAttribArray(glf::semantic::attr::POSITION);
+		glEnableVertexAttribArray(glf::semantic::attr::TEXCOORD);
+	glBindVertexArray(0);
+
+	return glf::checkError("initVertexArray");
+}
+
+bool begin()
+{
+	bool Validated = true;
+	Validated = Validated && glf::checkGLVersion(SAMPLE_MAJOR_VERSION, SAMPLE_MINOR_VERSION);
+	Validated = Validated && glf::checkExtension("GL_EXT_texture_compression_s3tc");
+	//Validated = Validated && glf::checkExtension("GL_ARB_compressed_texture_pixel_storage");
+
+	if(Validated)
+		Validated = initProgram();
+	if(Validated)
+		Validated = initArrayBuffer();
+	if(Validated)
+		Validated = initVertexArray();
+	if(Validated)
+		Validated = initTexture2D();
+
+	return Validated && glf::checkError("begin");
+}
+
+bool end()
+{
+	glDeleteBuffers(1, &BufferName);
+	glDeleteProgram(ProgramName);
+	glDeleteTextures(1, &Texture2DName);
+	glDeleteVertexArrays(1, &VertexArrayName);
+
+	return glf::checkError("end");
+}
+
+void display()
+{
+	// Compute the MVP (Model View Projection matrix)
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 1000.0f);
+	glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Window.TranlationCurrent.y));
+	glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Window.RotationCurrent.y, glm::vec3(1.f, 0.f, 0.f));
+	glm::mat4 View = glm::rotate(ViewRotateX, Window.RotationCurrent.x, glm::vec3(0.f, 1.f, 0.f));
+	glm::mat4 Model = glm::mat4(1.0f);
+	glm::mat4 MVP = Projection * View * Model;
+
+	glViewport(0, 0, Window.Size.x, Window.Size.y);
+	glClearBufferfv(GL_COLOR, 0, &glm::vec4(1.0f, 0.5f, 0.0f, 1.0f)[0]);
+
+	glUseProgram(ProgramName);
+	glUniformMatrix4fv(UniformMVP, 1, GL_FALSE, &MVP[0][0]);
+	glUniform1i(UniformDiffuse, 0);
+
+	glBindVertexArray(VertexArrayName);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Texture2DName);
+
+	glDrawArraysInstanced(GL_TRIANGLES, 0, VertexCount, 1);
+
+	glf::checkError("display");
+	glf::swapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	if(glf::run(
+		argc, argv,
+		glm::ivec2(::SAMPLE_SIZE_WIDTH, ::SAMPLE_SIZE_HEIGHT), 
+		::SAMPLE_MAJOR_VERSION, 
+		::SAMPLE_MINOR_VERSION))
+		return 0;
+	return 1;
+}
