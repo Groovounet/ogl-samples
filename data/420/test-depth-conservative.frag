9c3e1426473f5f473a613bf26477fcbc0cab2466
Christophe Riccio <mail@g-truc.net>
2011-08-03 01:15:27 +0100

    Added conservative depth sample

---
+++ data/420/test-depth-conservative.frag
@@ -0,0 +1,21 @@
+#version 420 core
+
+#define FRAG_COLOR		0
+
+precision highp float;
+precision highp int;
+
+uniform vec4 Diffuse;
+
+in block
+{
+	float Instance;
+} In;
+
+layout (depth_greater) out float gl_FragDepth;
+layout(location = FRAG_COLOR, index = 0) out vec4 Color;
+
+void main()
+{
+	Color = Diffuse * In.Instance * 0.25;
+}
---
+++ samples/ogl-420-test-depth-conservative.cpp
@@ -0,0 +1,175 @@
+//**********************************
+// OpenGL Samples Pack 
+// ogl-samples.g-truc.net
+//**********************************
+// OpenGL Test Depth Conservative
+// 03/08/2011 - 03/08/2011
+//**********************************
+// Christophe Riccio
+// ogl-samples@g-truc.net
+//**********************************
+// G-Truc Creation
+// www.g-truc.net
+//**********************************
+
+#include <glf/glf.hpp>
+
+namespace
+{
+	std::string const SAMPLE_NAME = "Test Depth Conservative";
+	std::string const VERTEX_SHADER_SOURCE(glf::DATA_DIRECTORY + "420/test-depth-conservative.vert");
+	std::string const FRAGMENT_SHADER_SOURCE(glf::DATA_DIRECTORY + "420/test-depth-conservative.frag");
+	int const SAMPLE_SIZE_WIDTH(640);
+	int const SAMPLE_SIZE_HEIGHT(480);
+	int const SAMPLE_MAJOR_VERSION(4);
+	int const SAMPLE_MINOR_VERSION(2);
+
+	glf::window Window(glm::ivec2(SAMPLE_SIZE_WIDTH, SAMPLE_SIZE_HEIGHT));
+
+	GLsizei const VertexCount(6);
+	GLsizeiptr const PositionSize = VertexCount * sizeof(glm::vec2);
+	glm::vec2 const PositionData[VertexCount] =
+	{
+		glm::vec2(-1.0f,-1.0f),
+		glm::vec2( 1.0f,-1.0f),
+		glm::vec2( 1.0f, 1.0f),
+		glm::vec2( 1.0f, 1.0f),
+		glm::vec2(-1.0f, 1.0f),
+		glm::vec2(-1.0f,-1.0f)
+	};
+
+	GLuint VertexArrayName(0);
+	GLuint ProgramName(0);
+	GLuint BufferName(0);
+	GLint UniformMVP(0);
+	GLint UniformDiffuse(0);
+}//namespace
+
+bool initTest()
+{
+	bool Validated = true;
+	glEnable(GL_DEPTH_TEST);
+
+	return Validated && glf::checkError("initTest");
+}
+
+bool initProgram()
+{
+	bool Validated = true;
+	
+	// Create program
+	if(Validated)
+	{
+		GLuint VertexShaderName = glf::createShader(GL_VERTEX_SHADER, VERTEX_SHADER_SOURCE);
+		GLuint FragmentShaderName = glf::createShader(GL_FRAGMENT_SHADER, FRAGMENT_SHADER_SOURCE);
+
+		ProgramName = glCreateProgram();
+		glAttachShader(ProgramName, VertexShaderName);
+		glAttachShader(ProgramName, FragmentShaderName);
+		glDeleteShader(VertexShaderName);
+		glDeleteShader(FragmentShaderName);
+
+		glLinkProgram(ProgramName);
+		Validated = glf::checkProgram(ProgramName);
+	}
+
+	// Get variables locations
+	if(Validated)
+	{
+		UniformMVP = glGetUniformLocation(ProgramName, "MVP");
+		UniformDiffuse = glGetUniformLocation(ProgramName, "Diffuse");
+	}
+
+	return Validated && glf::checkError("initProgram");
+}
+
+bool initArrayBuffer()
+{
+	glGenBuffers(1, &BufferName);
+    glBindBuffer(GL_ARRAY_BUFFER, BufferName);
+    glBufferData(GL_ARRAY_BUFFER, PositionSize, PositionData, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	return glf::checkError("initArrayBuffer");
+}
+
+bool initVertexArray()
+{
+	glGenVertexArrays(1, &VertexArrayName);
+    glBindVertexArray(VertexArrayName);
+		glBindBuffer(GL_ARRAY_BUFFER, BufferName);
+		glVertexAttribPointer(glf::semantic::attr::POSITION, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glEnableVertexAttribArray(glf::semantic::attr::POSITION);
+	glBindVertexArray(0);
+
+	return glf::checkError("initVertexArray");
+}
+
+bool begin()
+{
+	bool Validated = true;
+	Validated = Validated && glf::checkGLVersion(SAMPLE_MAJOR_VERSION, SAMPLE_MINOR_VERSION);
+	Validated = Validated && glf::checkExtension("GL_ARB_viewport_array");
+	Validated = Validated && glf::checkExtension("GL_ARB_separate_shader_objects");
+
+	if(Validated)
+		Validated = initTest();
+	if(Validated)
+		Validated = initProgram();
+	if(Validated)
+		Validated = initArrayBuffer();
+	if(Validated)
+		Validated = initVertexArray();
+
+	return Validated && glf::checkError("begin");
+}
+
+bool end()
+{
+	// Delete objects
+	glDeleteBuffers(1, &BufferName);
+	glDeleteProgram(ProgramName);
+	glDeleteVertexArrays(1, &VertexArrayName);
+
+	return glf::checkError("end");
+}
+
+void display()
+{
+	// Compute the MVP (Model View Projection matrix)
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Window.TranlationCurrent.y));
+	glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Window.RotationCurrent.y + 45.0f, glm::vec3(1.f, 0.f, 0.f));
+	glm::mat4 View = glm::rotate(ViewRotateX, Window.RotationCurrent.x + 45.0f, glm::vec3(0.f, 1.f, 0.f));
+	glm::mat4 Model = glm::mat4(1.0f);
+	glm::mat4 MVP = Projection * View * Model;
+
+	glProgramUniform4fv(ProgramName, UniformDiffuse, 1, &glm::vec4(1.0f, 0.5f, 0.0f, 1.0f)[0]);
+	glProgramUniformMatrix4fv(ProgramName, UniformMVP, 1, GL_FALSE, &MVP[0][0]);
+
+	glViewportIndexedf(0, 0, 0, float(Window.Size.x), float(Window.Size.y));
+
+	float Depth(1.0f);
+	glClearBufferfv(GL_DEPTH, 0, &Depth);
+	glClearBufferfv(GL_COLOR, 0, &glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)[0]);
+
+	glUseProgram(ProgramName);
+
+    glBindVertexArray(VertexArrayName);
+	glDrawArraysInstanced(GL_TRIANGLES, 0, VertexCount, 5);
+
+	glf::checkError("display");
+	glf::swapBuffers();
+}
+
+int main(int argc, char* argv[])
+{
+	if(glf::run(
+		argc, argv,
+		glm::ivec2(::SAMPLE_SIZE_WIDTH, ::SAMPLE_SIZE_HEIGHT), 
+		::SAMPLE_MAJOR_VERSION, 
+		::SAMPLE_MINOR_VERSION))
+		return 0;
+	return 1;
+}
