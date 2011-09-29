826ebb5365800f9b5da587be8836544f5b3915d3
Christophe Riccio <mail@g-truc.net>
2011-09-29 14:00:53 +0100

    Clean up

--- samples/ogl-330-primitive-point-sprite.cpp
+++ samples/ogl-330-primitive-point-sprite.cpp
@@ -156,8 +156,8 @@ bool begin()
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
 	glEnable(GL_PROGRAM_POINT_SIZE);
-	//glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT);
-	glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT);
+	glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_LOWER_LEFT);
+	//glPointParameteri(GL_POINT_SPRITE_COORD_ORIGIN, GL_UPPER_LEFT);
 	
 	if(Validated)
 		Validated = initTexture2D();
